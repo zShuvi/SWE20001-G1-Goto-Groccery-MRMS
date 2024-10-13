98d76173afabdc24437f1f9c3290f5e0ce68c64e
LiongJiaHwa <j22037228@student.newinti.edu.my>
2024-10-13 14:36:37 +0800

    Update adminsidebar.js

--- scripts/adminsidebar.js
+++ scripts/adminsidebar.js
@@ -11,14 +11,29 @@ if (sessionStorage.getItem('darkMode') === 'enabled') {
     modeText.innerText = "Light Mode"; // Adjust text if dark mode is enabled
 }
 
+// Check sessionStorage for sidebar state
+if (sessionStorage.getItem('sidebar_closed') === 'enabled') {
+    sidebar.classList.add("close");
+} else {
+    sidebar.classList.remove("close"); // Make sure to remove 'close' class if it's not enabled
+}
+
 // Toggle sidebar visibility
-toggle.addEventListener("click", () =>{
+toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
+
+    // Update sessionStorage based on the sidebar's state
+    if (sidebar.classList.contains("close")) {
+        sessionStorage.setItem('sidebar_closed', 'enabled');
+    } else {
+        sessionStorage.setItem('sidebar_closed', 'disabled');
+    }
 });
 
 // Open sidebar when search is clicked
 searchBtn.addEventListener("click", () =>{
     sidebar.classList.remove("close");
+    sessionStorage.setItem('sidebar_closed', 'disabled');
 });
 
 // Toggle dark mode and save the preference
