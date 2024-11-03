2467e7c14b8b7694ca7bee13a1c2cb2440bf0f87
LiongJiaHwa <j22037228@student.newinti.edu.my>
2024-11-03 15:42:48 +0800

    Update AdminEditUser.php for profile in sidebar

--- AdminEditUser.php
+++ AdminEditUser.php
@@ -41,7 +41,7 @@ $result = $conn->query($sql);
             <div class="image-text">
 
                 <span class="image">
-                    <img src="images/admin.png" alt="logo">
+                    <img src=<?php echo $_SESSION["profile_picture"]; ?> alt="Profile Picture" onerror="this.src='images/admin.png';">
                 </span>
 
 
