8108984507e329901f426bdc44f0c8167d44b81e
Dwarfy <j22036927@student.newinti.edu.my>
2024-11-13 12:40:19 +0800

    Update OrderHistory.php

--- OrderHistory.php
+++ OrderHistory.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css" rel="stylesheet">  
 	<link href="styles/orderhistory_style.css" rel="stylesheet">
-    <script defer src="scripts\orderhistory.js"></script> <!-- Link to JavaScript file -->
+    <script src="scripts\orderhistory.js"></script> <!-- Link to JavaScript file -->
 </head>
 
 <script type="text/javascript">
